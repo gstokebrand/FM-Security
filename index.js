f1550cee15e479c7686b0e3a6fedd7c49976b5dc
Gerwin Stokebrand <stokebrandgerwin@gmail.com>
2020-07-08 11:49:16 +0200

    added ${prefix}togglekick

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const modrole = botConfig.togglerole;
 const messagechannel = botConfig.channel;
 const kickchannel = botConfig.kickchannel;
 var kicktime = botConfig.defaultkicktime;
-var enabled = true;
+var infoenabled = true;
+var kickenabled = true;
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
@@ -51,9 +52,13 @@ client.on("message", message => {
             infochannel.send(embed)
         }
         if (message.member._roles.includes(modrole)) {
-            if (cmd == "toggle"){
-                enabled = ! enabled;
-                message.reply("joininfo is now: " + enabled)
+            if (cmd == "toggleinfo"){
+                infoenabled = ! infoenabled;
+                message.reply(`info on join is now: ${infoenabled}`)
+            }
+            if (cmd == "togglekick") {
+                kickenabled = ! kickenabled
+                message.reply(`kick if account age is now: ${kickenabled}`)
             }
             if (cmd =="kicktime") {
                 message.reply(`This command is not yet available. Kicktime = ${kicktime}`)
@@ -64,7 +69,7 @@ client.on("message", message => {
 
 client.on("guildMemberAdd", member => {
     console.log(member.user.tag + " just joined the server @ " + member.joinedAt)
-    if (enabled == true) {
+    if (infoenabled == true) {
         const infochannel = member.guild.channels.cache.find(ch => ch.name === messagechannel);
         let now = Date.now();
         let createdAt = member.user.createdTimestamp;
@@ -84,13 +89,16 @@ client.on("guildMemberAdd", member => {
     } else {
         console.log("Member joined but info is disabled.")
     }
-    if (Date.now() - member.user.createdAt < 1000*60*60*24*kicktime) {
+    if (Date.now() - member.user.createdAt < 1000*60*60*24*kicktime && kickenabled == true) {
         const kickmsgchannel = member.guild.channels.cache.find(ch => ch.name === kickchannel);
         const infochannel = member.guild.channels.cache.find(ch => ch.name === messagechannel);
         let age = Date.now() - member.user.createdTimestamp;
         member.user.send(`You have been kicked from ${message.member.guild.name}. Reason: Account too young. (${pm(age, { verbose: true })}). If you are a legitimate user come back when your account is at least ${kicktime} day(s) old.`);
         kickmsgchannel.send(`.kick ${member} Account not older than ${kicktime} day(s) (${pm(age, { verbose: true })})`);
+        console.log(`Kicked ${member.user.tag} for account age violation`)
         infochannel.send(`Kicked ${member} for account age violation (${pm(age, { verbose: true })})`)
+    } else if (Date.now() - member.user.createdAt < 1000 * 60 * 60 * 24 * kicktime && kickenabled == false) {
+        console.log(`${member.user.tag} joined with an account younger than ${kicktime} day(s). Kicking is disabled`)
     }
 });
 
